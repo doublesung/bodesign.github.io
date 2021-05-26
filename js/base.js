49b6f59c1982d90519e07c7ff5d85f436f04bca6
doublesung <57259598+doublesung@users.noreply.github.com>
2021-05-26 16:57:04 +0800

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    // nav
+    let $btnTop = $('#topBtn');
+    let exist = false;
+    // top btn
+    let $topBar = $('.topbar');
+    let $menuBtn = $('.menu-btn');
+    let menuOpen = false;
+    //footer
+    let $footer = $("footer");
+    let $footerBody = $(".footer-body");
+
+    function loadOver (){
+        
+        $(window).scrollTop(0);
+        
+        $topBar.animate({ top: 0, opacity: 1 }, 1000);
+    }
+    
+    loadOver ();
+
+    $(window).scroll(function(){
+        scrollFn();
+    })
+
+    function scrollFn () {
+        let $windowT = $(window).scrollTop();
+        let $windowH = $(window).height();
+        //footer
+        let $footerTop = $("footer").offset().top;
+        
+        if( $windowT > $windowH / 2) {
+            $btnTop.css({"opacity":"1","cursor":"pointer"});
+            exist = true;
+        }else {
+            $btnTop.css({"opacity":"0","cursor":"default"})
+            exist = false;
+        }
+
+        console.log( $windowT + $windowH, $footerTop)
+        // animation
+        if( $windowT + $windowH > $footerTop + 100 ){
+            $footer.animate({ opacity: 1}, 600);
+            $footerBody.animate({top: 0}, 600);
+        }
+
+    }
+
+    $menuBtn.click(function(){
+        if(!menuOpen){
+            $menuBtn.addClass('open');
+            menuOpen = true; 
+        }else {
+            $menuBtn.removeClass('open');
+            menuOpen = false;
+        }
+    })
+
+    $btnTop.click(function(){
+        if(exist){
+            $(window).scrollTop(0);
+        }
+    })
+})
---
+++ js/plan.js
@@ -0,0 +1,43 @@
+// about
+let $aboutTop =  new Array();
+let $aboutContent = new Array();
+let $aboutImg = new Array();
+
+$aboutTop[0] = $(".about:eq(0)").offset().top;
+$aboutContent[0] = $(".about:eq(0) .about-content");
+$aboutImg[0] = $(".about:eq(0) .about-img");
+
+for(let i = 1; i < 4; i++){
+    $aboutContent[i] = $(".about:eq("+i+") .about-content");
+    $aboutImg[i] = $(".about:eq("+i+") .about-img");
+}
+
+$aboutContent[0].animate({ opacity: 1, left: 0 }, 500);
+$aboutImg[0].animate({ opacity: 1,  left: 0 }, 800);
+
+ $(window).scroll(function(){
+    scrollFn();
+})
+
+function scrollFn () {
+    let $windowT = $(window).scrollTop();
+    let $windowH = $(window).height();
+    
+    // animation
+    for(let i = 1; i < 4; i++){
+
+        for(let i = 1; i < 4; i++){
+            $aboutTop[i] = $(".about:eq("+i+")").offset().top;
+        }
+
+        if( $windowT + $windowH > $aboutTop[i] + 250){
+            $aboutContent[i].animate({ opacity: 1, left: 0 }, 500);
+        }
+    
+        if( $windowT + $windowH > $aboutTop[i] + 350){
+            $aboutImg[i].animate({ opacity: 1,  left: 0 }, 800);
+        }
+
+        
+    }
+}
